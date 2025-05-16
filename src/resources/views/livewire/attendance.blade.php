e8abd0f5b88cc7b868e0bac4b546c48a7c0c8aae
hiroko4ikeda8 <art_stylist_spinning@yahoo.co.jp>
2025-05-17 00:43:43 +0900

    dashboardファイルにattendance.blade.phpを埋め込んでみた

--- src/resources/views/livewire/attendance.blade.php
+++ src/resources/views/livewire/attendance.blade.php
@@ -1,5 +1,3 @@
-
-
 <div>
     <div class="min-h-screen flex flex-col lg:flex-row lg:justify-center lg:items-center pt-6 sm:pt-0">
         <div class="w-full sm:max-w-md lg:max-w-2xl mt-6 px-12 py-8 bg-white shadow-md overflow-hidden sm:rounded-lg">
